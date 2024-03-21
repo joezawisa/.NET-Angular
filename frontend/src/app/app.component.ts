a0becfaa94c54cd9504c940b115aab5962577325
Joe Zawisa <36611576+joezawisa@users.noreply.github.com>
2024-03-20 21:05:30 -0400

    Fetch albums from backend
    Instead of using a static array of albums on the front end,
    get the list of albums from the back end. We've successfully integrated a C#
    back end with an Angular front end!

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BrowserModule],
+  imports: [RouterOutlet, BrowserModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  albums: Array<Album> = [
-    {id: 1, name: 'One'},
-    {id: 2, name: 'Two'}
-  ]
+  albums: Array<Album> = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Array<Album>>('/albums').subscribe((data) => {
+      this.albums = data;
+    })
+  }
 }
 
 interface Album {
