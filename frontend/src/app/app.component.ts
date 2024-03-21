65428bdfda5f9de892ad3ba2b489afdf1b1fd391
Joe Zawisa <36611576+joezawisa@users.noreply.github.com>
2024-03-20 20:55:19 -0400

    Display array of album names
    Show a a list of album names that comes from an array.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BrowserModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'photos';
+  albums: Array<Album> = [
+    {id: 1, name: 'One'},
+    {id: 2, name: 'Two'}
+  ]
+}
+
+interface Album {
+  id: number,
+  name: string
 }
